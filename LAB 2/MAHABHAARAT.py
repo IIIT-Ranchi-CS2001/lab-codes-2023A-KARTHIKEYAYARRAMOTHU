65bda53e8790d38b05369bdc03472718c2a6c855
KARTHIKEYAYARRAMOTHU <yarramothu.2023ug1048@iiitranchi.ac.in>
2024-09-03 17:05:07 +0530

    LAB 2

---
+++ LAB 2/MAHABHAARAT.py
@@ -0,0 +1,8 @@
+s1 ='Maha Bharat'
+
+print(s1.swapcase()) 
+print(s1[5:])
+print(s1.replace('Maha','Mera'))
+print(s1.replace('Maha','Mera')+(' Mahan')) 
+
+print(s1[5:]*3)
---
+++ LAB 2/PALINDROME.PY
@@ -0,0 +1,6 @@
+s = "NITIN"
+r = s[::-1]
+if (s==r):
+    print("The string is a palindrome.")
+else:
+    print("The string is not a palindrome.")
